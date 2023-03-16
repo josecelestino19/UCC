23e59873665db6372076c20be48e0bfd26e5c243
jose.celestino <jose.celestino@cassca.com>
2023-03-17 00:13:56 +0100

    vucc-versao15

---
+++ projecto_universidade_DEMO_15/js/customizar.js
@@ -0,0 +1,5 @@
+ /*HORAS AUTOMATICAS*/
+ var data=new Date();
+ var ano=data.getFullYear();
+ var direitos=window.document.getElementById('copy');
+ copy.innerHTML=`Copyright© ${ano} | Universidade Cuito Cuanavale`;
